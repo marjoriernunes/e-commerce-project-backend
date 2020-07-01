32d515abae60353d0d9be5288e50b55e09cf62f5
Marjorie Nunes Ribeiro <marjorie.ribeiro@bgmrodotec.com.br>
2020-06-30 21:03:55 -0300

    add maven project

---
+++ miss-marge/src/main/java/com/ecommerce/missmarge/MissMargeApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.missmarge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissMargeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissMargeApplication.class, args);
+	}
+
+}
---
+++ miss-marge/src/test/java/com/ecommerce/missmarge/MissMargeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.missmarge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissMargeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
