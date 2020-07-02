3abbc966d3d158cb6b72e18813c327b60ed58bde
Marjorie Nunes Ribeiro <marjorie.ribeiro@bgmrodotec.com.br>
2020-07-01 22:21:44 -0300

    update project

---
+++ miss-marge/src/main/java/com/ecommerce/missmarge/model/Produto.java
@@ -0,0 +1,86 @@
+package com.ecommerce.missmarge.model;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import javax.persistense.Id;
+import javax.persistense.Column;
+
+@Entity(name = produto)
+public class Produto {
+	@Id
+	private Long id;
+	
+	@Column(name = quantidade_estoque)
+	private BigInteger quantidadeEstoque;
+	
+	@Column(name = nome_produto)
+	private String nomeProduto;
+	
+	@Column(name = descricao_produto)
+	private String descricaoProduto;
+	
+	@Column(name = valor_produto)
+	private BigDecimal valorProduto;
+	
+	@Column(name = caminho_imagem)
+	private String caminhoImagem;
+	
+	@Column(name = descricao_imagem)
+	private String descricaoImagem;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public BigInteger getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+
+	public void setQuantidadeEstoque(BigInteger quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public String getDescricaoProdutoString() {
+		return descricaoProdutoString;
+	}
+
+	public void setDescricaoProdutoString(String descricaoProdutoString) {
+		this.descricaoProdutoString = descricaoProdutoString;
+	}
+
+	public BigDecimal getValorProduto() {
+		return valorProduto;
+	}
+
+	public void setValorProduto(BigDecimal valorProduto) {
+		this.valorProduto = valorProduto;
+	}
+
+	public String getCaminhoImagem() {
+		return caminhoImagem;
+	}
+
+	public void setCaminhoImagem(String caminhoImagem) {
+		this.caminhoImagem = caminhoImagem;
+	}
+
+	public String getDescricaoImagem() {
+		return descricaoImagem;
+	}
+
+	public void setDescricaoImagem(String descricaoImagem) {
+		this.descricaoImagem = descricaoImagem;
+	}
+}
