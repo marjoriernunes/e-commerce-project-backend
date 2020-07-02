7168cbfaca443e3a2cbe93e4257f4361c4c8f1af
Marjorie Nunes Ribeiro <marjorie.ribeiro@bgmrodotec.com.br>
2020-07-01 23:05:46 -0300

    initial services

--- miss-marge/src/main/java/com/ecommerce/missmarge/controller/ProdutoController.java
+++ miss-marge/src/main/java/com/ecommerce/missmarge/controller/ProdutoController.java
@@ -1,16 +1,30 @@
 package com.ecommerce.missmarge.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.missmarge.repository.ProdutoRepository;
 
 @RestController
 public class ProdutoController {
 	
-	@GetMapping(path = "/api/listaProdutos")
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@GetMapping(path = "/api/lista-produtos")
 	public void listaProdutos() {}
 	
-	@PostMapping
+//	@GetMapping(path = "/api/historico-compras/{}")
+//	public void historicoCompras(
+//			@PathVariable(name = "id", required = true) Long idUsuario) {
+//		return ResponseEntity.ok(body);
+//	}
+	
+	@PostMapping(path = "/api/cadastrar-carrinho-compras/produtos")
 	public void cadastrarCarrinhoCompras() {}
 	
 }
